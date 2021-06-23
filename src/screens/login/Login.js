e2405bd801a53e2a67020e3ed14f2d3222e83e72
Sanatt Abrol <sanatt.abrol.in@gmail.com>
2021-06-24 00:09:17 +0530

    Added required validation on the login form

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,34 +1,64 @@
 import React,{Component} from 'react';
 import Header from '../../common/header/Header';
-import {Card, CardContent, Typography, FormControl, Input, InputLabel, Button} from '@material-ui/core';
+import {Card, CardContent, Typography, FormControl, Input, InputLabel, Button, FormHelperText} from '@material-ui/core';
 import './Login.css';
 
 class Login extends Component{
-  render(){
-      return(
+    
+    constructor() {
+        super();
+        this.state = { 
+          username: "",
+          usernameRequired: "dispNone",
+          password: "",
+          passwordRequired: "dispNone",
+        }
+    }
+
+    inputUsernameChangeHandler = (event) => {
+       this.setState({username:event.target.value});
+    }
+    
+    inputPasswordChangeHandler = (event) => {
+        this.setState({password:event.target.value});
+    }
+
+    loginClickHandler = () => {
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
+        this.state.password === "" ? this.setState({passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" });
+    }
+
+    render(){
+        return(
             <div>
                 <Header title="Image Viewer"/>
-                <div>
-                    <Card className="login-card">
+                <div className = "card-container">
+                    <Card className = "login-card">
                         <CardContent>
-                            <Typography variant="headline" component="h2"> LOGIN </Typography><br/>
+                            <Typography variant="h5"> LOGIN </Typography><br/>
                             <FormControl required>
                                 <InputLabel htmlFor="username">Username</InputLabel>
-                                <Input id="username" type="text"></Input>
+                                <Input type = "text" id = "username" username = {this.state.username} onChange = {this.inputUsernameChangeHandler}></Input>
+                                <FormHelperText  className = {this.state.usernameRequired}>
+                                    <span className = "required-warning">required</span>    
+                                </FormHelperText>
                             </FormControl> 
                             <br /><br />
                             <FormControl required>
                                 <InputLabel htmlFor="password">Password</InputLabel>
-                                <Input id="password" type="password"></Input>
+                                <Input type = "password" id = "password" password = {this.state.password} onChange = {this.inputPasswordChangeHandler}></Input>
+                                <FormHelperText className = {this.state.passwordRequired}>
+                                    <span className = "required-warning">required</span>    
+                                </FormHelperText>
                             </FormControl>
                             <br /><br /><br />
-                            <Button variant="contained" color="primary">LOGIN</Button>
+                            <Button variant="contained" color="primary" onClick = {this.loginClickHandler} >LOGIN</Button>
                         </CardContent>
                     </Card>
                 </div>
             </div>
-      )
-  }
+        )
+    }
 }
 
 export default Login;
